903e1686a8c92c25375ab10afac4a9c7886e2dc0
Matt Senn <msenn2@uncc.edu>
2011-09-07 13:19:08 -0400

    Maximum overbug

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int sudoku[9][9][10], solve();
-void output(), update_others(), reset_sudoku(), initial_update();
+void output(), update_others(), reset_sudoku(), initial_update(), reset_all_dependancies();
 
 int main()
 {
@@ -23,7 +22,7 @@ int main()
 	
 	initial_update();
         
-	if (solve(0, 0))
+	if (solve(0, 0) == 1)
 		puts("we somehow solved it!");
 	else
 		puts("shit, its not solved");
@@ -40,7 +39,7 @@ int solve(int i, int j)
 	if (i == 9)		//if i mod 9 is 0, increment j.
 	{	i = 0;	
 		if (++j == 9)		//if after j is incremented, j is 9, return true.
-			return true;	//the puzzle is solved.
+			return 1;	//the puzzle is solved.
 	}
 	if (sudoku[i][j][0] != 0) 		//if cell filled
 		return solve(i+1, j);	//solve next position (and exit this function immediately with result)
@@ -49,13 +48,13 @@ int solve(int i, int j)
 	
 	int value;
 	for(value = 1; value <= 9; value++)
-		if (valid(i, j, value))
+		if (valid(i, j, value) == 1)
 		{
 			printf("Trying %d\n", value);
 			sudoku[i][j][0] = value;		//we're going to try this value out
 			update_others(i, j);	//update all it is affecting
-			if (solve(i+1, j))	//if solving the puzzle with this value works
-			   return true;		//return true (puzzle is completely solved)
+			if (solve(i+1, j) == 1)	//if solving the puzzle with this value works
+			   return 1;		//return true (puzzle is completely solved)
 			/*else continue;*/	//fun optional line. (if solving worked, return true. else, continue solving)
 		}
 		else
@@ -63,9 +62,23 @@ int solve(int i, int j)
 			printf("%d tested invalid.\n",value); 
 		}
 
+	puts("A value didn't work.");
 	//you're still here? nothing we tried worked....
 	sudoku[i][j][0] = 0;	//reset the value
-	return false;		//return false (puzzle not solved)
+	reset_all_dependancies();
+	return 0;		//return false (puzzle not solved)
+}
+
+void reset_all_dependancies()
+{
+	puts("About to reset all");
+	int x, y, z;
+	for(x = 0; x < 9; x++)
+	for(y = 0; y < 9; y++)
+	for(z = 1; z < 10; z++)
+		sudoku[x][y][z] = 1;
+
+	initial_update();
 }
 
 void output()
@@ -128,13 +141,17 @@ void update_others(i, j)
 	if (j > 5) n = 6;
 	
 	if (sudoku[i][j][0] == 0) { printf("In update_others with %d,%d and shouldn't be", i, j); return;}
-	printf("Updating others. sudoku[%d][%d][0]=%d ", i, j, sudoku[i][j][0]);
+	printf("Updating others. sudoku[%d][%d][0]=%d\n ", i, j, sudoku[i][j][0]);
 	for(x = 0; x < 9; x++)
 	{
 		sudoku[i][x][ sudoku[i][j][0] ] = 0;
 		sudoku[x][j][ sudoku[i][j][0] ] = 0;
 		sudoku[m+x/3][n+x%3][ sudoku[i][j][0] ] = 0;
-		printf("(%d,%d)",m+x/3,n+x%3);
+		//printf("(%d,%d)",m+x/3,n+x%3);
+		
+		printf("sudoku[%d][%d][0]=%d --> sudoku[%d][%d][%d]=0\n",i,j,sudoku[i][j][0],i,x,sudoku[i][j][0]);
+		printf("sudoku[%d][%d][0]=%d --> sudoku[%d][%d][%d]=0\n",i,j,sudoku[i][j][0],x,j,sudoku[i][j][0]);
+		printf("sudoku[%d][%d][0]=%d --> sudoku[%d][%d][%d]=0\n",i,j,sudoku[i][j][0],m+x/3,n+x%3,sudoku[i][j][0]);
 	}
 	puts("");
 
@@ -144,5 +161,6 @@ void update_others(i, j)
 
 int valid(int i, int j, int value)
 {
+	printf("sudoku[%d][%d][%d]=%d\n",i,j,value,sudoku[i][j][value]);
 	return sudoku[i][j][value];
 }
