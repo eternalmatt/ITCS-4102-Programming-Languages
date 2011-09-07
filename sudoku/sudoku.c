7e6ecfcc13561790306342bbc9b29f3899b80393
Matt Senn <msenn2@uncc.edu>
2011-09-07 00:54:13 -0400

    File input and stdout working properly.
    However, the algorithm doesn't actually work....
    Seems like I've got a bit to do. I have a feeling that its
    returning true early and not actually doing much.
    
    Maximum debugging about to commence.

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -1,9 +1,39 @@
-
+#include <stdio.h>
+#include <stdbool.h>
 
 int sudoku[9][9][10];
+void output(), update_others();
+int solve();
+int main()
+{
+	FILE* file = fopen("sudoku.txt", "r");
+	if (file == NULL) return;
+	
+	puts("getting input");
+
+	int i, j;
+	for(i = 0; i < 9; i++)
+	    for(j = 0; j < 9; j++)
+		fscanf(file, "%d", sudoku[i][j]);
+	
+	fclose(file);
+	
+	puts("about to do output");
+        if (solve(0, 0))
+		puts("we somehow solved it!");
+	else
+		puts("shit, its not solved");
+
+	output();
+
+	puts("Still here!");
+
+}
 
 
-BOOL solve(int i, int j)
+
+
+int solve(int i, int j)
 {
 	if ( (i %= 9) == 0)		//if i mod 9 is 0, increment j.
 		if (++j == 9)		//if after j is incremented, j is 9, return true.
@@ -16,18 +46,33 @@ BOOL solve(int i, int j)
 	for(value = 1; value <= 9; value++)
 		if (valid(i, j, value))
 		{
-			sudoku[i][j] = value;		//we're going to try this value out
-			update_possibilities(i, j);	//update all it is affecting
+			sudoku[i][j][0] = value;		//we're going to try this value out
+			update_others(i, j);	//update all it is affecting
 			if (solve(i+1, j))	//if solving the puzzle with this value works
 			   return true;		//return true (puzzle is completely solved)
 			/*else continue;*/	//fun optional line. (if solving worked, return true. else, continue solving)
 		}
 
 	//you're still here? nothing we tried worked....
-	sudoku[i][j] = 0;	//reset the value
+	sudoku[i][j][0] = 0;	//reset the value
 	return false;		//return false (puzzle not solved)
 }
 
+void output()
+{
+	int i, j;
+	for(i = 0; i < 9; i++){
+	for(j = 0; j < 9; j++)
+		printf("%d", sudoku[i][j][0]);
+	putchar('\n');}
+}
+
+/* simple method to reset sudoku array
+ *
+ * basically follows like so: 
+ * forall x,y in sudoku[i][j][0] = 0
+ * forall x,y and z from (1-9) in sudoku[x][y][z] = 1
+ */
 void reset_sudoku()
 {
 	int x, y, z;
@@ -40,12 +85,14 @@ void reset_sudoku()
 	}
 }
 
+/*quickly loop through sudoku array and update all possibilities
+*/
 void initial_update()
 {
 	int i, j;
 	for(i = 0; i < 9; i++)
 	for(j = 0; j < 9; j++)
-	    if (sudoku[i][j]) 
+	    if (sudoku[i][j][0]) 
 		update_others(i, j);
 }
 
@@ -65,16 +112,16 @@ void update_others(i, j)
 	int m, n, x;
 	for(m=i%3, n=j%3, x = 0; x < 9; x++)
 	{
-		sudoku[i][x][sudoku[i][j]] = 0;
-		sudoku[x][j][sudoku[i][j]] = 0;
-		sudoku[m+x/3][n+x%3][sudoku[i][j]] = 0;
+		sudoku[i][x][sudoku[i][j][0]] = 0;
+		sudoku[x][j][sudoku[i][j][0]] = 0;
+		sudoku[m+x/3][n+x%3][sudoku[i][j][0]] = 0;
 	}
 
 }
 
 
 
-BOOL valid(int i, int j, value)
+int valid(int i, int j, int value)
 {
 	return sudoku[i][j][value];
 }
