a12ae04de768b9ea13f114658499c444336ddd75
Matt Senn <eternalmatt@gmail.com>
2011-09-08 15:32:05 -0400

    Haskell source compiles
    Also, git is being weird. Not sure if I'm committing twice or not.
    I'll just look at it on github later where I can actally see the
    branches.
    
    Oh, and Haskell source doesn't do anything yet. Just got it to
    compile, that's all.

--- sudoku/hasdoku.hs
+++ sudoku/hasdoku.hs
@@ -1,22 +1,23 @@
 
 
+
+
+getRow :: [[Int]] -> Int -> [Int]
+getRow grid n = grid !! n
+
+getCol :: [[Int]] -> Int -> [Int]
+getCol grid n = [ row !! n | row <- grid]
+
+getBox :: [[Int]] -> Int -> Int -> [Int]
+getBox grid i j = let threeRows = take 3 $ map (getRow grid) [3*(i`div`3)..]
+                  in  concat $ take 3 $ drop (3*(j`div`3)) threeRows
+--getBox grid (x,y) = [ take 3 (drop (3*(x-1)) row) | row <- take 3 (drop (3*(y-1)) grid)]
+{-
 type Grid = [[Int]]
 type Cell = Int
 type Row = [Int]
 type Col = [Int]
-type Box = [[Int]]
-
-getRow :: Grid -> Int -> Row
-getRow grid n = grid !! n
-
-getCol :: Grid -> Int -> Col
-getCol grid n = [ row !! n | row <- grid]
-
-getBox :: Grid -> Int -> Int -> Box
-getBox grid i j = let threeRows = take 3 $ map (getRow grid) [3*(i`div`3)..]
-                  in  take 3 $ map (getCol grid) [3*(j`div`3)..]
---getBox grid (x,y) = [ take 3 (drop (3*(x-1)) row) | row <- take 3 (drop (3*(y-1)) grid)]
-
+type Box = [Int]
 allRows :: Grid -> [Row]
 allRows = id
 
@@ -32,21 +33,16 @@ partSolved part = sort part == [1..9]
 
 allSolved :: Grid -> Bool
 allSolved grid = all (map (all . partSolved) [allRows, allCols, concat allBoxes])
+-}
 
 
-solve :: Grid -> Int -> Int -> Maybe Grid
+solve :: [[Int]] -> Int -> Int -> Maybe [[Int]]
 solve grid 8 8 = Just grid
 solve grid i 9 = solve grid (i+1) 0
 solve grid i j
-   | position grid i j /= 0 = solve i (j+1)
-   | case fancy of []      -> Nothing
-                   solution -> Just solution
-     where fancy = head [ solved | value <- [1..9], valid value && solved /= Nothing
-                          let newGrid = replaceIndex grid i j value
-                              valid   = all $ map (notElem value) [getRow i, getCol j, getBox (i`div` 3) (j `div `3)]
-                              solved  = solve newGrid i (j+1)
-                        ]
-
-
-
-
+   | position grid i j /= 0 = solve grid i (j+1)
+   | otherwise = head [ solved | value <- [1..9], let solved  = solve (replaceIndex grid i j value) i (j+1), (and ( map (notElem value) [getRow grid i, getCol grid j, getBox grid i j])) && solved /= Nothing]
+                      
+position i j k = 0
+replaceIndex i j k l = [[0]]
+                      
