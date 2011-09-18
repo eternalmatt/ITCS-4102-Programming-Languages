be0bed9e6f9791c5ea87aadffd36395f4da4e88a
Matt Senn <eternalmatt@gmail.com>
2011-09-18 16:55:39 -0400

    Communicates via stdin/stdout operates accordingly.
    You know, a lot of this code is just having to coerrse the input
    to be proper, but in haskell its usually really concise.  Little
    snippets like
    
    	map read . reverse $ line file
    
    They just continue to amaze me. Anyway this will be nearly my final
    edit unless I just get bored and keep moving stuff around.

--- crypto/hascryption.hs
+++ crypto/hascryption.hs
@@ -1,20 +1,46 @@
-import Data.List
+fileKey = "key.txt"
+filePlain = "plain.txt"
+fileEncryption = "encryption.txt"
 
-main = putStr $ concatMap (trueDecrypt thekey . trueEncrypt thekey) secrets
+main = do
+   putStrLn "Would you like encryption or decryption (e/d)?"
+   key <- readFile fileKey
+   choice <- getLine
+   whatdo choice
+   
+whatdo :: String -> IO ()
+whatdo "encryption" = whatdo "e"
+whatdo "e" = do plain <- readFile filePlain
+                key   <- readFile fileKey
+                let encryptions = encryptMessages (read key) plain
+                    output = unlines (map show encryptions)
+                putStrLn output
+whatdo "decryption" = whatdo "d"
+whatdo "d" = do file <- readFile fileEncryption
+                key  <- readFile fileKey
+                let encryptions = map read . reverse $ lines file
+                    messages = decryptMessages (read key) encryptions
+                putStrLn messages
+whatdo _ = do putStrLn "Couldn't understand the input."
+              main
+   
+decryptMessages :: Integer -> [Integer] -> String
+decryptMessages key = reverse . concat . map (decrypt key)
+
+encryptMessages :: Integer -> String -> [Integer]
+encryptMessages key = map (encrypt key) . split22
 
 
-decrypt,trueDecrypt :: Integer -> Integer -> String
-trueDecrypt key message = reverse (decrypt key message)
-
-decrypt key message 
+decrypt :: Integer -> Integer -> String
+decrypt key message
    | message <= key = []
    | otherwise      = letter : decrypt key ((message - intChar) `div` 128)
      where intChar = message `div` key `mod` 128
            letter  = charFromInt intChar
 
 
-trueEncrypt :: Integer -> String -> Integer    
-trueEncrypt key message = case reverse message of 
+encrypt :: Integer -> String -> Integer    
+encrypt key message = case reverse message of 
                  (c:cs) ->       key * intFromChar c + encrypt cs
    where encrypt (c:cs) = 128 * (key * intFromChar c + encrypt cs)
          encrypt []     = 0
@@ -25,24 +51,16 @@ ascii = [(toEnum 0)..]
            
 charFromInt :: Integer -> Char
 charFromInt n = ascii `genericIndex` n
+   where genericIndex (c:_) 0 = c
+         genericIndex (_:s) i = genericIndex s (i-1)
 
 intFromChar :: Char -> Integer
 intFromChar c = ascii `indexOf` c
    where indexOf xs y = head [ i | (x,i)<-zip xs [0..], x == y]
 
 
-thekey   =  43925490168452731
-messages = [657283544027693586405973913936912676341316860151136582564041440,
-            762158440814722218409670997866763993970236423491053273021270170,
-            253262132952333831995121662054324568193258875445201983148665941,
-            860669930383051094828380046079622968863517085136410890595760608,
-            781811748281830352989488557417573583693051102964224942664590125,
-            822416390125976509670768141097965886409376847628987159121865489,
-            915523982326070161546651135364185543215004770371941496548944981,
-            911051292099529766286289435282947264489230853142015083465572192]
-secret = "Sometimes you can see a lot just by looking. (Yogi Berra)\nAn economist is an expert who can explain tomorrow why\nhis previous predictions for today did not happen."
-secrets :: [String]
-secrets = init' ++ [last']
+split22 :: String -> [String]
+split22 secret = init' ++ [last']
    where phrases = splitBy 22 secret
          init' = init phrases
          last' = take 22 $ last phrases ++ repeat ' '
@@ -59,5 +77,17 @@ encrypt,trueEncrypt :: Integer -> String -> Integer
 encrypt key []     = 0
 encrypt key (c:cs) = 128 * (key * intChar + encrypt key cs)
                      where intChar = intFromChar c
+
+--some variables used during debugging                     
+thekey   =  43925490168452731
+messages = [657283544027693586405973913936912676341316860151136582564041440,
+            762158440814722218409670997866763993970236423491053273021270170,
+            253262132952333831995121662054324568193258875445201983148665941,
+            860669930383051094828380046079622968863517085136410890595760608,
+            781811748281830352989488557417573583693051102964224942664590125,
+            822416390125976509670768141097965886409376847628987159121865489,
+            915523982326070161546651135364185543215004770371941496548944981,
+            911051292099529766286289435282947264489230853142015083465572192]
+secret = "Sometimes you can see a lot just by looking. (Yogi Berra)\nAn economist is an expert who can explain tomorrow why\nhis previous predictions for today did not happen."
 -}
 
