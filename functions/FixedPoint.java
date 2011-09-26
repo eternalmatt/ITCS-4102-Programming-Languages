73a4d218231344d124ad34e94b2d0c213d7d4556
Matt Senn <eternalmatt@gmail.com>
2011-09-26 00:31:19 -0400

    Update to comply with spec

--- functions/FixedPoint.java
+++ functions/FixedPoint.java
@@ -4,14 +4,11 @@ public class FixedPoint
 {
 	double[][] a;
 	double[]   b;
-	F f = new F();
-	G g = new G();
-	H h = new H();
 	Vector start;
 	
 	public class Vector
 	{
-		public double x,y,z;
+		private final double x,y,z;
 		public Vector(double x, double y, double z)
 		{
 			this.x=x;this.y=y;this.z=z;
@@ -26,68 +23,63 @@ public class FixedPoint
 		
 		public String toString()
 		{
-			return "n:" + count + "\tx:" + x + "\ty:" + y + "\tz:" + z;
+			return "\tx:" + x + "\ty:" + y + "\tz:" + z;
 		}
 	}
 	
 	public FixedPoint(double[][] a, double[] b, double x, double y, double z)
 	{
-		this.a = a; this.b = b;
+		this.a = a; 
+		this.b = b;
 		start = new Vector(x,y,z);
 	}
 	
-	public String go()
+	public void go()
 	{
-		return iterate(start).toString();
-	}
-	private int count = 0;
-	public Vector iterate(Vector xyz)
-	{
-		System.out.println(xyz);
-		if (++count == 100)
-			return xyz;
+	 	F f = new F();
+		G g = new G();
+		H h = new H();
 		
-		Vector next = new Vector(
-			f.fixer(xyz),
-		   g.fixer(xyz),
-		   h.fixer(xyz)
-		);
-		
-		if (xyz.equals(next))
-			return next;
-		else
-			return iterate(next);
-	}
+		Vector vec = start;
+		double x,y,z;
 	
-	
-	public abstract class AbstractFunction
-	{
-		public abstract double fixer(Vector lol);
-	}
-	
-	public class F extends AbstractFunction
-	{
-		public double fixer(Vector xyz)
+		for(int n=1; n<=100; n++)
 		{
+			x = calculate(f, vec);
+			y = calculate(g, vec);
+			z = calculate(h, vec);
+			
+			vec = new Vector(x,y,z);
+			
+			System.out.print("n:" + n);
+			System.out.println(vec);
+		}
+		
+	}
+	
+	public double calculate(AbstractFunction function, Vector xyz)
+	{
+		return function.fixer(xyz);
+	}
+	
+	
+	public abstract class AbstractFunction{
+		public abstract double fixer(Vector v);
+	}
+	
+	public class F extends AbstractFunction{
+		public double fixer(Vector xyz){
 			return (b[1] - a[1][2]*xyz.y - a[1][3]*xyz.z) / a[1][1];
-		}
-	}
+	}}
 	
-	public class G extends AbstractFunction
-	{
-		public double fixer(Vector xyz)
-		{
+	public class G extends AbstractFunction{
+		public double fixer(Vector xyz){
 			return (b[2] - a[2][1]*xyz.x - a[2][3]*xyz.z) / a[2][2];
-		}
-	}
+	}}
 	
-	public class H extends AbstractFunction
-	{
-		public double fixer(Vector xyz)
-		{
+	public class H extends AbstractFunction{
+		public double fixer(Vector xyz){
 			return (b[3] - a[3][1]*xyz.x - a[3][2]*xyz.y) / a[3][3];
-		}
-	}
-	
+	}}
 	
 }
--- functions/HigherOrder.java
+++ functions/HigherOrder.java
@@ -9,11 +9,11 @@ public class HigherOrder
 	   //input = keyboard.readLine();
 		
 //		double[][] a = new double[][]{{0,0,0,0},{0,5,2,9},{0,3,-7,8},{0,-2,4,-10}};
-		double[][] a = new double[][]{{0,0,0,0},{0,5,3,-2},{0,2.0,-7.0, 4.0},{0,9.0, 8.0, -10.0}};
+		double[][] a = new double[][]{{0,0,0,0},{0,5,3,-2},{0,2,-7,4},{0,9,8,-10}};
 		
 		double[] b = new double[]{0,13,21,-6};
 		
 		FixedPoint lolPoint = new FixedPoint(a,b,0,1,2);
-		System.out.println( lolPoint.go());
+		lolPoint.go();
 	}
 }
