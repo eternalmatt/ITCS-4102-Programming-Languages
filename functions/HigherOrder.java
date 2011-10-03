5b0fc0c0590b4b7c8faaa0908643697e52d0b548
Matt Senn <eternalmatt@gmail.com>
2011-10-03 13:02:27 -0300

    readFromFile method added for easy file input.

--- functions/HigherOrder.java
+++ functions/HigherOrder.java
@@ -3,17 +3,38 @@ import java.util.*;
 
 public class HigherOrder
 {
+	static double a[][], b[];
+
 	public static void main(String[] args) throws IOException
 	{
 		//BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 	   //input = keyboard.readLine();
 		
-//		double[][] a = new double[][]{{0,0,0,0},{0,5,2,9},{0,3,-7,8},{0,-2,4,-10}};
-		double[][] a = new double[][]{{0,0,0,0},{0,5,3,-2},{0,2,-7,4},{0,9,8,-10}};
-		
-		double[] b = new double[]{0,13,21,-6};
+		readFromFile("constants.txt");
 		
 		FixedPoint lolPoint = new FixedPoint(a,b,0,1,2);
 		lolPoint.go();
+	}
+	
+	static void readFromFile(String name) throws FileNotFoundException
+	{
+		Scanner sc = new Scanner(new File(name));
+				
+		a = new double[3][3];
+		for(int x=0; x<3; x++)
+		for(int y=0; y<3; y++)
+		{
+			a[x][y] = sc.nextFloat();
+			System.out.println("A["+x+"]["+y+"] = "+a[x][y]);
+		}
+			
+		b = new double[3];
+		for(int x=0; x<3; x++)
+		{
+			b[x] = sc.nextFloat();
+			System.out.println("B["+x+"] = "+b[x]);
+		}
+
+		sc.close();
 	}
 }
